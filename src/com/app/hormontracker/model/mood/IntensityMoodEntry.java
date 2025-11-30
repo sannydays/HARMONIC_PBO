cc861af3b13e6e6eb13a2e65bf2bb0a9750b17e6
sannydays <skiyaaaf@gmail.com>
2025-11-30 09:36:08 +0700

    upd comment

--- src/com/app/hormontracker/model/mood/IntensityMoodEntry.java
+++ src/com/app/hormontracker/model/mood/IntensityMoodEntry.java
@@ -6,12 +6,12 @@ public class IntensityMoodEntry extends MoodEntry {
     private final int intensity;
 
     public IntensityMoodEntry(LocalDate date, MoodType type, String note, int intensity) {
-        super(date, type, note); // Call parent constructor
+        super(date, type, note); // Memanggil parent constructor
         this.intensity = intensity;
     }
 
     @Override
-    public int getMoodLevel() {
+    public int getMoodLevel() { // Setter untuk mengakses intensitas mood
         return intensity;
     }
 }
